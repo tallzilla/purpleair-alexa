170a02136d2375dd76e728d2895792db10ccfed0
Bill Heil <bill@billheil.com>
2020-10-17 23:03:30 -0700

    back to the main file

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,1 @@
-if len(logging.getLogger().handlers) > 0:
-    # The Lambda environment pre-configures a handler logging to stderr. If a handler is already configured,
-    # `.basicConfig` does not execute. Thus we set the level directly.
-    root = logging.getLogger()
-    if root.handlers:
-        for handler in root.handlers:
-            root.removeHandler(handler)
-    logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
-else:
-    logging.basicConfig(level=logging.INFO)
+pass
