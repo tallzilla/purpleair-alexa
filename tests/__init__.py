61197e6f0ffeabfa174dfc0928343fb19ddc0f19
Bill Heil <bill@billheil.com>
2020-10-17 22:57:50 -0700

    logging woes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,1 @@
-import logging
-if len(logging.getLogger().handlers) > 0:
-    # The Lambda environment pre-configures a handler logging to stderr. If a handler is already configured,
-    # `.basicConfig` does not execute. Thus we set the level directly.
-    logging.getLogger().setLevel(logging.INFO)
-else:
-    logging.basicConfig(level=logging.INFO)
+pass
